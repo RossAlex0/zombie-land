3e955fc21b2e0e6c2b89a233397ed0acda8a8e61
Rossignol Alex <alexnw33910@gmail.com>
2026-05-18 13:33:22 +0200

    feat(prettier-eslint): init eslint and prettier config

--- src/utils/hooks/hook.ts
+++ src/utils/hooks/hook.ts
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 export const useBjr = () =>
   React.useCallback(
     () =>
-      fetch("/api/activities")
+      fetch('/api/activities')
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
         }),
-    [],
+    []
   );
