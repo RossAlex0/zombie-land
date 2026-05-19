9615379b0603e66707d9e9cf0338ff1aacd42677
Herve <148484299+H-Nuggets@users.noreply.github.com>
2026-05-19 15:50:09 +0200

    feat: signup back

---
+++ src/app/api/(auth)/signup/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { IUserSignup } from '../../../../utils/types/User';
+
+import argon2 from 'argon2';
+import { prisma } from '@prismaInstance/*';
+
+export async function POST(req: NextRequest) {
+  try {
+    const userData: IUserSignup = await req.json();
+    if (!userData) {
+      throw new Error('User data missing');
+    }
+    const { password, confirmPassword, ...userToCreate } = userData;
+    if (password !== confirmPassword) throw new Error('Les mots de passes ne correspondent pas');
+
+    const hash = await argon2.hash(password);
+
+    const addedUser = prisma.user.create({ data: { ...userToCreate, password: hash } });
+    console.log('added user to db :', addedUser);
+
+    const response = NextResponse.json({ message: 'Inscription réussie' }, { status: 200 });
+
+    return response;
+  } catch (err) {
+    console.error(err);
+    const response = NextResponse.json(
+      { message: "Problème lors de l'inscription" },
+      { status: 400 }
+    );
+    return response;
+  }
+}
