670fea3617b1e7fb76522a2d04d0858bf09487f9
Rossignol Alex <alexnw33910@gmail.com>
2026-05-21 09:37:56 +0200

    feat(back-office): created back office page

---
+++ src/app/admin/back-office/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@components/block/header/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Header />
+      </main>
+    </div>
+  );
+}
