7fb462bf750cf7e08c655c0fcdcd2697debff1a5
Rossignol Alex <alexnw33910@gmail.com>
2026-05-21 09:09:21 +0200

    feat(sass): add and init scss

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), 'src/utils/styles')],
+
+  },
 };
 
 export default nextConfig;
