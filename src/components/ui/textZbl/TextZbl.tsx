9b3c5fa7623806578c1a7330770f75a38534ae73
Rossignol Alex <alexnw33910@gmail.com>
2026-05-21 09:11:10 +0200

    feat(components): create text zbl components

---
+++ src/components/ui/textZbl/TextZbl.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './TextZbl.scss';
+
+export type TextZblProps = {
+  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'p';
+  jetbrains?: boolean;
+  color?: 'white' | 'black' | 'yellow' | 'red' | 'grey' | 'none';
+  redPrefix?: string;
+  children: React.ReactNode;
+} & React.HTMLAttributes<HTMLElement>;
+
+export default function TextZbl({
+  tag: Tag = 'p',
+  color = 'white',
+  jetbrains = false,
+  redPrefix,
+  children,
+  ...props
+}: TextZblProps) {
+  const fontText = jetbrains ? '_jtb' : undefined;
+
+  const colorText = color !== 'none' && `color_${color}`;
+  return (
+    <Tag
+      {...props}
+      className={`text_${Tag}${fontText} ${colorText} ${props.className ?? ''}`.trim()}
+    >
+      {redPrefix ? <span className="prefix color_red text_p_jtb">{redPrefix}</span> : undefined}
+      {children}
+    </Tag>
+  );
+}
