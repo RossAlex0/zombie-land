0a31f51c99de651bb2a2a383687f22ff7dbbcddf
Rossignol Alex <alexnw33910@gmail.com>
2026-05-21 14:59:44 +0200

    feat(stat-card): created start card component

---
+++ src/components/block/statCards/StatCards.tsx
@@ -0,0 +1,39 @@
+import TextZbl from '@components/ui/textZbl/TextZbl';
+import StatCard from './StatCard';
+
+import './statCards.scss';
+
+export type Stat = {
+  label: string;
+  value: number;
+  lowLevel: number;
+  highLevel: number;
+  unit?: string;
+};
+
+type StatCardsProps = {
+  stats: Stat[];
+};
+
+export default function StatCards({ stats }: StatCardsProps) {
+  return (
+    <section className="stat_cards">
+      <TextZbl jetbrains className="stat_cards_note">
+        *Les chiffres affichés sont calculés suivant la date choisie
+      </TextZbl>
+
+      <div className="stat_cards_grid">
+        {stats.map((stat) => (
+          <StatCard
+            key={stat.label}
+            label={stat.label}
+            value={stat.value}
+            unit={stat.unit}
+            lowLevel={stat.lowLevel}
+            highLevel={stat.highLevel}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
