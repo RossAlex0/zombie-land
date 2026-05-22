8887039bec08d6d9c8460f15c86dedf013657331
Rossignol Alex <alexnw33910@gmail.com>
2026-05-22 10:32:29 +0200

    feat(auth-layout): add icons into auth layout

--- src/components/layout/authLayout/AuthLayout.tsx
+++ src/components/layout/authLayout/AuthLayout.tsx
@@ -1,8 +1,42 @@
+'use client';
+import TextZbl from '@components/ui/textZbl/TextZbl';
+import Image from 'next/image';
 import './authLayout.scss';
+import { useRouter } from 'next/navigation';
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return (
     <main className="auth_main">
-      <section className="auth_main_content">{children}</section>
+      <section className="auth_main_content">
+        <div className="auth_main_content_header">
+          <span className="auth_main_content_header_span">
+            <TextZbl jetbrains redPrefix="//">
+              Zone_infectée_001&nbsp;&nbsp;
+            </TextZbl>
+            <TextZbl jetbrains redPrefix="//">
+              Foyer_012
+            </TextZbl>
+          </span>
+          <TextZbl jetbrains redPrefix="//">
+            Formulaire_access_142
+          </TextZbl>
+          <div className="auth_main_logo">
+            <div className="auth_main_logo_image" onClick={() => router.push('/')}>
+              <Image fill src={'/icons/logo.svg'} alt="Montagne russe zombie" loading="eager" />
+            </div>
+            <div className="auth_main_logo_text">
+              <TextZbl tag="h2" color="none" className="auth_main_title">
+                ZOMBIE LAND
+              </TextZbl>
+              <TextZbl>
+                Le dernier parc d’attractions encore ouvert après l’effondrement nucléaire !
+              </TextZbl>
+            </div>
+          </div>
+        </div>
+        <div>{children}</div>
+      </section>
     </main>
   );
 }
