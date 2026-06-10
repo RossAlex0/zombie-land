61eef4f1e8a50cd49ce5fda6a0d6db5d90d73d06
LudivineV01 <veloso.ludivine@gmail.com>
2026-06-10 18:24:31 +0200

    feat(bo): add backofficefield and backofficevalue shared components

--- src/utils/types/User.d.ts
+++ src/utils/types/User.d.ts
@@ -10,3 +10,14 @@ export interface IUserSignup extends IUser {
   password: string;
   confirmPassword: string;
 }
+
+export interface IUserBO {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  role: { name: string };
+  valid_email: boolean | null;
+  birth_date: string | null;
+  created_at: string;
+}
